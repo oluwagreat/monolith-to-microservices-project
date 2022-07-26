584986c15effbe19ff027ae70a0bab38c642ccbe
oluwagreat <oluwatobiloba1@gmail.com>
2022-07-26 11:41:51 +0100

    Rebuilt frontend image

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a754cd9fc42de48fea8078e1efc2de9c-2036846516.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
